82d02ab9da9b96782ae5f50bf8a74661e39c8e64
me00000000000 <cipherwave2025@gmail.com>
2025-03-22 20:47:57 +0100

    add mix-paranoid, and fix design mistake which needed specific file to decrypt
    now, you can just decrypt with mix and it will detect the header

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 cp mix /usr/bin/
-cp mix-dump /usr/bin/
 cp mix-lite /usr/bin/
+cp mix-paranoid /usr/bin/
+cp mix-dump /usr/bin/
 chmod +x /usr/bin/mix
-chmod +x /usr/bin/mix-dump
 chmod +x /usr/bin/mix-lite
+chmod +x /usr/bin/mix-paranoid
+chmod +x /usr/bin/mix-dump

f3c08e2cd650730d1c8a3d84e4d3820d733725a9
me00000000000 <cipherwave2025@gmail.com>
2025-03-23 17:31:02 +0100

    add uninstall.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-cp mix /usr/bin/
-cp mix-lite /usr/bin/
-cp mix-paranoid /usr/bin/
-cp mix-dump /usr/bin/
-chmod +x /usr/bin/mix
-chmod +x /usr/bin/mix-lite
-chmod +x /usr/bin/mix-paranoid
-chmod +x /usr/bin/mix-dump
+if test $(basename $0) == "install.sh"
+then
+	{
+		cp -v mix /usr/bin/
+		cp -v mix-lite /usr/bin/
+		cp -v mix-paranoid /usr/bin/
+		cp -v mix-dump /usr/bin/
+		chmod +x /usr/bin/mix
+		chmod +x /usr/bin/mix-lite
+		chmod +x /usr/bin/mix-paranoid
+		chmod +x /usr/bin/mix-dump
+	} && {
+		echo "successfully installed mix"
+	}
+else
+	{
+		rm -v /usr/bin/mix 
+		rm -v /usr/bin/mix-lite 
+		rm -v /usr/bin/mix-paranoid
+		rm -v /usr/bin/mix-dump
+	} && {
+		echo "successfully removed mix"
+	}
+fi
